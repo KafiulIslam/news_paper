978a2cb55ef56749483c40c5c86a199e87e9fbe3
Md. Kafiul Islam <90089792+KafiulIslam@users.noreply.github.com>
2022-01-19 23:13:38 -0800

    Delete android_time_tracker directory
    delete the directory to open new repo

--- android_time_tracker/lib/app/sign_in/home/entry_job.dart
+++
@@ -1,9 +0,0 @@
-import 'package:android_time_tracker/app/sign_in/home/model/job.dart';
-import 'package:android_time_tracker/app/sign_in/home/model/entry.dart';
-
-class EntryJob {
-  EntryJob(this.entry, this.job);
-
-  final Entry entry;
-  final Job job;
-}
--- android_time_tracker/test/validators_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:android_time_tracker/app/sign_in/validators.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('non empty string', () {
-    final validator = NonEmptyStringValidator();
-    expect(validator.isValid('test'), true);
-  });
-}
